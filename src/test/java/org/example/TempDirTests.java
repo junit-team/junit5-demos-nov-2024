eb8dc524ef168fae48f0be7ed2eb8b4e4f1e048a
Marc Philipp <mail@marcphilipp.de>
2024-11-25 11:19:42 +0100

    Add another test using `TempDir`

--- src/test/java/org/example/TempDirTests.java
+++ src/test/java/org/example/TempDirTests.java
@@ -5,17 +5,41 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TempDirTests {
 
+    @TempDir
+    Path tempDir;
+
     @Test
-    void test(@TempDir Path tempDir) throws Exception {
+    void canReadAndWriteTextFile() throws Exception {
         var testFile = tempDir.resolve("test.txt");
 
         Files.writeString(testFile, "Hello, World!");
 
         assertEquals("Hello, World!", Files.readString(testFile));
     }
+
+    @Test
+    void canReadAndWriteBinaryFile() throws Exception {
+        var testFile = tempDir.resolve("test.out");
+        int sizeInKiB = 1024;
+
+        try (var outputStream = Files.newOutputStream(testFile)) {
+            var random = new Random();
+            var bufferSizeInKiB = 8;
+            var buffer = new byte[bufferSizeInKiB * 1024];
+            for (var i = 0; i < (sizeInKiB / bufferSizeInKiB); i++) {
+                random.nextBytes(buffer);
+                outputStream.write(buffer);
+            }
+        }
+
+        var sizeInBytes = sizeInKiB * 1024;
+        assertEquals(sizeInBytes, Files.size(testFile));
+    }
+
 }
