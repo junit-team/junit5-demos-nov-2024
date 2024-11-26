cf784b9d446d97d69418810c29e85ffb37759b09
Marc Philipp <mail@marcphilipp.de>
2024-11-26 12:44:18 +0100

    Expand `Timeout` example

--- src/test/java/org/example/timeout/TimeoutTests.java
+++ src/test/java/org/example/timeout/TimeoutTests.java
@@ -1,31 +1,50 @@
 package org.example.timeout;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.CountDownLatch;
 
 @Timeout(1)
 public class TimeoutTests {
 
     @Test
     void fast() {
-        printJUnitThreads();
+        printJUnitThreads("fast()");
     }
 
     @Test
     void slow() {
-        printJUnitThreads();
+        printJUnitThreads("slow()");
         try {
             Thread.sleep(5_000);
         } catch (InterruptedException e) {
             System.out.println("Interrupted on thread: " + Thread.currentThread().getName());
+//            waitIndefinitely();
         }
     }
 
-    private static void printJUnitThreads() {
-        System.out.println("Active threads:");
+    @AfterAll
+    static void afterAll() {
+        printJUnitThreads("@AfterAll");
+    }
+
+    private static void printJUnitThreads(String header) {
+        System.out.printf("--- %s ---%n", header);
+        System.out.println("JUnit threads:");
         Thread.getAllStackTraces().keySet().stream()
                 .filter(it -> it.getName().equals("main") || it.getName().startsWith("junit-"))
-                .forEach(it -> System.out.println("- " + it.getName()));
+                .forEach(it -> System.out.printf("- %s (%s)%s%n", it.getName(), it.getState(), it == Thread.currentThread() ? " <-- current" : ""));
         System.out.println();
+    }
+
+    @SuppressWarnings("unused")
+    private static void waitIndefinitely() {
+        try {
+            new CountDownLatch(1).await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
