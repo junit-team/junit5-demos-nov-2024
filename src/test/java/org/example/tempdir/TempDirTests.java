9167f8dc41907226e0321d867009b5e231c8e1e6
Marc Philipp <mail@marcphilipp.de>
2024-11-27 09:51:45 +0100

    Remove distracting second test method

--- src/test/java/org/example/tempdir/TempDirTests.java
+++ src/test/java/org/example/tempdir/TempDirTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,25 +24,6 @@ class TempDirTests {
         Files.writeString(testFile, "Hello, World!");
 
         assertEquals("Hello, World!", Files.readString(testFile));
-    }
-
-    @Test
-    void canReadAndWriteBinaryFile() throws Exception {
-        var testFile = tempDir.resolve("test.out");
-        int sizeInKiB = 1024;
-
-        try (var outputStream = Files.newOutputStream(testFile)) {
-            var random = new Random();
-            var bufferSizeInKiB = 8;
-            var buffer = new byte[bufferSizeInKiB * 1024];
-            for (var i = 0; i < (sizeInKiB / bufferSizeInKiB); i++) {
-                random.nextBytes(buffer);
-                outputStream.write(buffer);
-            }
-        }
-
-        var sizeInBytes = sizeInKiB * 1024;
-        assertEquals(sizeInBytes, Files.size(testFile));
     }
 
 }
